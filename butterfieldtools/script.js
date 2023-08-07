042ae7e95ee3a4e7001b79b46c565cb3ed6dd509
thetronic <stevetronic@gmail.com>
2023-08-07 13:32:23 +0100

    With PDF working and highlighting

--- butterfieldtools/script.js
+++ butterfieldtools/script.js
@@ -1,37 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-  function downloadRtf(content) {
-    const rtfContent = `{{\\rtf1\\ansi\\deff0
-{\\colortbl;\\red0\\green0\\blue0;}
-${content.replace(/\n/g, '\\par\n')}
-}}`
-
-    const blob = new Blob([rtfContent], { type: 'text/rtf' })
-    const downloadLink = document.createElement('a')
-    downloadLink.href = URL.createObjectURL(blob)
-    downloadLink.download = 'modified_text.rtf'
-
-    document.body.appendChild(downloadLink)
-    downloadLink.click()
-
-    document.body.removeChild(downloadLink)
-  }
-
   document
     .getElementById('submitButton')
     .addEventListener('click', function () {
       const inputText = document.getElementById('textInput').value
 
-      const regexPattern = /({.*})|(<KEYNOTE - .*>)|((##)(\n|.)*?(##))/g
+      const regexPattern = /({.*})|(<.*>)|((##)(\n|.)*?(##))/g
       const modifiedText = inputText.replace(regexPattern, '')
       const cleanedText = modifiedText.replace(/\n{3,}/g, '\n')
-
-      const boldedRtfContent = cleanedText.replace(/<([^>]*)>/g, '{\\b $1 }')
 
       // Create a Blob with the cleaned text for TXT file
       const txtBlob = new Blob([cleanedText], { type: 'text/plain' })
       const txtDownloadLink = document.createElement('a')
       txtDownloadLink.href = URL.createObjectURL(txtBlob)
-      txtDownloadLink.download = 'modified_text.txt'
+      txtDownloadLink.download = 'Cue Script.txt'
 
       // Append the link to the DOM and trigger a click event to initiate the download
       document.body.appendChild(txtDownloadLink)
@@ -40,23 +21,37 @@ ${content.replace(/\n/g, '\\par\n')}
       // Clean up the temporary link
       document.body.removeChild(txtDownloadLink)
 
-      // Download the RTF file
-      downloadRtf(boldedRtfContent)
-
       // Generate and download PDF
-      const docDefinition = {
-        content: [
-          {
-            text: cleanedText.replace(/<([^>]*)>/g, (match, group) => ({
-              text: group,
+      const pdfContent = []
+      const regexPatternPDF = /({.*})|(<KEYNOTE - .*>)|((##)(\n|.)*?(##))/g
+      const modifiedTextPDF = inputText.replace(regexPatternPDF, '')
+      const cleanedTextPDF = modifiedTextPDF.replace(/\n{3,}/g, '\n')
+      let lineNumber = 1
+      cleanedTextPDF.split(/<([^>]*)>/).forEach((part, index) => {
+        if (index % 2 === 0) {
+          pdfContent.push(part)
+        } else {
+          if (pdfContent.length > 0) {
+            pdfContent.push('', {
+              text: `${lineNumber++}. ${part}`,
               bold: true,
-            })),
-            margin: [0, 10],
-          },
-        ],
+              background: 'yellow',
+            })
+          } else {
+            pdfContent.push({
+              text: `${lineNumber++}. ${part}`,
+              bold: true,
+              background: 'yellow',
+            })
+          }
+        }
+      })
+
+      const docDefinition = {
+        content: [{ text: pdfContent, margin: [0, 10] }],
       }
 
       const pdfDocGenerator = pdfMake.createPdf(docDefinition)
-      pdfDocGenerator.download('modified_text.pdf')
+      pdfDocGenerator.download('Tech Script.pdf')
     })
 })
